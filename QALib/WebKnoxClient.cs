7efff30efe9d605bd2c90a3ec223e6b63db46bb3
Sunil Pottumuttu <mailtop.sunil@gmail.com>
2016-08-22 18:08:27 +0530

    Initial Version

---
+++ QALib/WebKnoxClient.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace QALib
+{
+    public class WebKnoxClient
+    {
+        private string __apiKey;
+
+        public WebKnoxClient( string apiKey)
+        {
+            this.__apiKey = apiKey;
+
+        }
+
+
+        public WebKnoxResponse Query(string query)
+        {
+            var apiKey = HttpUtility.UrlEncode(this.__apiKey);
+            var q = HttpUtility.UrlEncode(query);
+
+            var uri = $"http://webknox.com/api/questions/answers?question={q}&answerLookup=false&answerSearch=false&apiKey={apiKey}";
+
+            string json;
+            using (HttpClient client = new HttpClient())
+            {
+                json = client.GetStringAsync(uri).Result; 
+            }
+            try
+            {
+                var response = JsonConvert.DeserializeObject<WebKnoxResponse>(json);
+                return response;
+            }
+            catch (JsonException ex)
+            {
+                return null;
+            }
+        }
+
+        
+    }
+}
---
+++ QASystemConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QALib;
+using static System.Console;
+using System.Configuration;
+
+namespace QASystemConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var apiKey = ConfigurationManager.AppSettings["apikey"].ToString();
+            WebKnoxClient client = new WebKnoxClient(apiKey);
+            
+            WriteLine("Please Enter A Factoid Question(Ex:what is csharp)");
+            string question = ReadLine();
+
+            question = question.Trim();
+
+            var response = client.Query(question);
+            WriteLine(response.Answer);
+            ReadLine();
+        }
+    }
+}
